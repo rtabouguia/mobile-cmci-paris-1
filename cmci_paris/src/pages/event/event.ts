73d2d89e48e72b94340aa195a8282a723ebb54ef
Victorien TEDONGMEZA <victorientedongmeza@sfeirMAC85.local>
2018-08-21 21:40:19 +0200

    Ajout du menu du bas avec leur page associée

---
+++ cmci_paris/src/pages/event/event.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-event',
+  templateUrl: 'event.html'
+})
+export class EventPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
